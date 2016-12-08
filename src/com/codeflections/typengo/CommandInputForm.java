3d4358a8f305c7ad5ad79821ef55aef6c2ccbdb2
Rustam Vishnyakov <rustam.vishnyakov@jetbrains.com>
2016-12-08 15:08:47 +0300

    Cleanup

--- src/com/codeflections/typengo/CommandInputForm.java
+++ src/com/codeflections/typengo/CommandInputForm.java
@@ -35,8 +35,6 @@ import org.jetbrains.annotations.Nullable;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Collection;
@@ -72,22 +70,16 @@ public class CommandInputForm extends JDialog {
         topPanel.setBorder(BorderFactory.createLineBorder(JBColor.gray));
         this.setAlwaysOnTop(true);
         commandField.setRequestFocusEnabled(true);
-        commandField.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        commandField.addActionListener(e -> {
 
-            }
         });
         KeyStroke escKeyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0, false);
-        commandField.registerKeyboardAction(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                popupMenu.setVisible(false);
-                CommandInputForm.this.setVisible(false);
-                CommandInputForm.this.dispose();
-                currTyped = null;
-                focusOnIdeFrame(ideFrame);
-            }
+        commandField.registerKeyboardAction(e -> {
+            popupMenu.setVisible(false);
+            CommandInputForm.this.setVisible(false);
+            CommandInputForm.this.dispose();
+            currTyped = null;
+            focusOnIdeFrame(ideFrame);
         }, escKeyStroke, JComponent.WHEN_FOCUSED);
         commandField.addKeyListener(new KeyListener() {
             @Override
@@ -147,7 +139,7 @@ public class CommandInputForm extends JDialog {
         }
     }
 
-    private boolean focusOnIdeFrame(@Nullable JFrame ideFrame) {
+    private void focusOnIdeFrame(@Nullable JFrame ideFrame) {
         if (ideFrame != null) {
             if (SystemInfo.isLinux) {
                 if (!ideFrame.isFocused()) {
@@ -165,9 +157,7 @@ public class CommandInputForm extends JDialog {
             } catch (InterruptedException e) {
                 // Ignore
             }
-            return true;
         }
-        return false;
     }
 
     @NotNull
